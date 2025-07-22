0b0492636232eb0de7696cda26d28486f2d9fe9f
coo1ifyy <114829930+coo1ifyy@users.noreply.github.com>
2025-07-22 16:56:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -383,7 +383,6 @@ client.on('interactionCreate', async (interaction) => {
       embeds: [embed],
       components: interaction.message.components,
     });
-  }
 })
 
 client.login(process.env.BOT_TOKEN)
